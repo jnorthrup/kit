f4b0d81891fd8dea52002c42fa9436a1dc8e4039
Jim Northrup <northrup.james@gmail.com>
2012-08-27 09:08:02 -0700

    works on a mysql instance

--- kit/src/com/vsiwest/kit/excel/ExcelAccessor.java
+++
@@ -1,9 +0,0 @@
-package com.vsiwest.kit.excel;
-
-/**
- * (c) Copyright 2011 vsiwest, Inc.  All rights reserved.
- * Confidential Information.  Do not copy or distribute without express written permission.
- */
-public interface ExcelAccessor<T> {
-    T getTypedValue();
-}
